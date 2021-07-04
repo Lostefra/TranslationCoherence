6da54cc2270ee7166bd23c4b5fc9eaa71a11c7d2
Andrea Lavista <andrea.lavista@studio.unibo.it>
2021-07-04 23:14:16 +0200

    Add textual references

--- build_graph.py
+++ build_graph.py
@@ -1,12 +1,17 @@
+import rdflib.term
 from rdflib import Graph
 
+WORD_REFERENCE_PREFIX = "offset_"
+DENOTE_PROPERTY = "http://ontologydesignpatterns.org/cp/owl/semiotics.owl#denotes"
+HAS_INTERPRETANT_PROPERTY = "http://ontologydesignpatterns.org/cp/owl/semiotics.owl#hasInterpretant"
+TEXTUAL_REFERENCE_PROPERTY = rdflib.term.URIRef("http://example.org/translation_coherence/textualReference")
 UNWANTED = ["http://www.ontologydesignpatterns.org/ont/fred/pos.owl",
             "http://ontologydesignpatterns.org/cp/owl/semiotics.owl",
             "http://www.essepuntato.it/2008/12/earmark",
             "http://www.ontologydesignpatterns.org/ont/cnlp/dependencies.owl",
             "http://www.ontologydesignpatterns.org/ont/vn/data",
             "http://www.ontologydesignpatterns.org/ont/boxer/boxer.owl",
-            "offset_",
+            WORD_REFERENCE_PREFIX,
             "ObjectProperty"]
 
 
@@ -40,9 +45,24 @@ def graph_bind(g_clean):
     g_clean.bind("transl_coher", "http://example.org/translation_coherence/")
 
 
+#add to each wanted element the related word (if exists)
+#TO DISCUSS: this function could encapsulate the cleaning
+def construct_text_references(g_in):
+    g_out = Graph()
+    for s, p, o in g_in:
+        #check if the subject is an offset in the ontology and the property a reference to individuals or classes
+        if WORD_REFERENCE_PREFIX in s and (str(p) == DENOTE_PROPERTY or str(p) == HAS_INTERPRETANT_PROPERTY):
+                #get the expression
+                expression = ' '.join((' '.join(s.split('_')[3:])).split('+'))
+                new_triple = (o, TEXTUAL_REFERENCE_PROPERTY, rdflib.term.Literal(expression))
+                g_out.add(new_triple)
+        g_out.add((s, p, o))
+    return g_out
+
 def build_graph(path):
     g_raw = Graph()
     g_raw.parse(path, format="turtle")
-    g_clean = clean_graph(g_raw)
+    g_enriched = construct_text_references(g_raw)
+    g_clean = clean_graph(g_enriched)
     graph_bind(g_clean)
     return g_clean
